06a750453c0a0de7478eff3e5d8f71c0325d7c40
bimaaryawijaya <102515996+bimaaryawijaya@users.noreply.github.com>
2023-03-01 20:25:19 +0700

    Add files via upload

---
+++ PRAKTIKUM 2/OperasiTitik.java
@@ -0,0 +1,29 @@
+/**
+* File		: OperasiTitik.java
+* Penulis	: Bima Arya Wijaya
+* Deskripsi : kelas berisi 
+*/
+class OperasiTitik{
+	private void refleksiSumbuX(Titik titik){
+		double ordinat = titik.getOrdinat();
+		ordinat = ordinat*-1;
+		titik.setOrdinat(ordinat);
+	}
+	
+	private void refleksiSumbuY(Titik titik){
+		double absis = titik.getAbsis();
+		absis = absis*-1;
+		titik.setAbsis(absis);
+	}
+	
+	public Titik refleksiX(Titik titik){
+		refleksiSumbuX(titik);
+		return titik;
+	}
+	
+	public Titik refleksiY(Titik titik){
+		refleksiSumbuY(titik);
+		return titik;
+	}
+	
+}
