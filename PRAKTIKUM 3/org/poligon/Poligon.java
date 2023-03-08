48deb359f412f0f1bbbbee7de038a5de01eaf4e8
bimaaryawijaya <102515996+bimaaryawijaya@users.noreply.github.com>
2023-03-08 19:41:54 +0700

    Add files via upload

---
+++ PRAKTIKUM 3/org/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/**
+* File		: Poligon.java
+* Penulis	: Bima Arya Wijaya
+* Deskripsi : representasi dasar dari objek poligon (segi banyak)
+*/
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
