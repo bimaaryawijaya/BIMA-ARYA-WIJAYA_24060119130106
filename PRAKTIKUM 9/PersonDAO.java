6fb1352722a48178c78150e893ad8f7e82d07066
bimaaryawijaya <102515996+bimaaryawijaya@users.noreply.github.com>
2023-05-31 19:47:25 +0700

    Add files via upload

---
+++ PRAKTIKUM 9/PersonDAO.java
@@ -0,0 +1,8 @@
+/**
+* File		: PersonDAO.java
+* Penulis	: Bima Arya Wijaya
+* Deskripsi : interface untuk person access object
+*/
+public interface PersonDAO{
+	public void savePerson(Person P) throws Exception;
+}
